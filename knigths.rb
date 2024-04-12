94bdb412c0817e71e49dd9d80ede40d340a2d01a
Alexander Hoang <Aleexx.hoang@gmail.com>
2024-04-12 21:18:03 +0200

    built the method correctSpot?

--- knigths.rb
+++ knigths.rb
@@ -1,4 +1,5 @@
 #Create constants that set the rules for how the knight can move, example [2, 1. -2, -1 etc].
+#Create constants that set the rules for how the knight can move, example [2, 1. -2, -1 etc].
 #Build a function knight_moves which accepts starting positiion and ending position, have a stack and add every possioble position to the queue.
 #Keep track of visited positions on the board, prevent the knight from going to the same place again
 
@@ -37,50 +38,41 @@ def knight_moves(startpos, endpos)
 
     while q.length > 0
 
-
         prev = q.shift
-        return if prev.value == endpos
+
         #This is where the Knight determines which steps to include in the stack next, this is done by creating knight objects with the next steps
-
         
-        ### Figure out why val = 0,0 and prevval = nil appears multiple times
-
         LEGAL_MOVES.each do |val|
             temp = val.map.with_index { |val, idx| prev.value[idx] + val
                 (prev.value[idx] + val)}
 
-        ### try to check node in already visited instead of temp
             if valid_move?(temp) && already_visited?(temp)
                 knight = Knight.new(temp, prev)
+                return if correctSpot?(knight, endpos) == true
                 q << knight
             end
         end
 
-        
-        ###### WORK HERE #######
-
-        
-        #if knight.value == endpos
-        #    while knight.prevValue == true
-        #        puts knight.prevValue
-        #        tempKnight = knight.prevValue
-        #    end
-        #end
-        ###### WORK HERE #######
-
-        ######
-        #Create an if statement to check wheter to create new steps, or if the endpos is already reached
-        #If the endpos is already reached, returned the current value and prevvalues
-
-        ######
-
-
         #p Knight.class_variable_get(:@@visited)
-        
-        
-
+        p q
     end
-    
+end
+
+def correctSpot?(knight, endpos)
+
+    if knight.value == endpos
+
+        currentKnight = knight
+        tempKnight = ""
+
+        while currentKnight.prevValue != nil
+            p currentKnight.value
+            tempKnight = currentKnight.prevValue
+            currentKnight = tempKnight
+        end
+    return true
+    end
+
 end
 
 def valid_move?(val)
@@ -92,7 +84,4 @@ def already_visited?(val)
 end
 
 
-
-knight_moves([0,0], [2, 1])
-p Knight.all
-
+knight_moves([0,0], [4, 2])
